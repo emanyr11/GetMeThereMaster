4b4239a9790fa5f3b4905ab7cc5604da0b2b6f24
llNitishAnandll <nz.nitish@hotmail.co.nz>
2017-06-03 22:10:34 +1200

    There are three different markers for each campus.

--- app/src/main/java/com/GetMeThere/GetMeThereLogin/MapsActivity.java
+++ app/src/main/java/com/GetMeThere/GetMeThereLogin/MapsActivity.java
@@ -1,5 +1,7 @@
 package com.GetMeThere.GetMeThereLogin;
 
+
+import android.location.LocationManager;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 
@@ -14,33 +16,34 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     private GoogleMap mMap;
 
+    LocationManager locationManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
     }
 
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng AUT = new LatLng(-36.9855, 174.8819);
-        mMap.addMarker(new MarkerOptions().position(AUT).title("AUT SOUTH CAMPUS"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(AUT));
+        //Makers for Constant Destinations
+        LatLng SouthAUT = new LatLng(-36.9855, 174.8819);
+        LatLng CityAUT = new LatLng(-36.8532, 174.7673);
+        LatLng NorthAUT = new LatLng(-36.7979, 174.7583);
+
+        mMap.addMarker(new MarkerOptions().position(SouthAUT).title("AUT SOUTH CAMPUS"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(SouthAUT, 10.2f));
+
+        mMap.addMarker(new MarkerOptions().position(CityAUT).title("AUT CITY CAMPUS"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(SouthAUT, 10.2f));
+
+        mMap.addMarker(new MarkerOptions().position(NorthAUT).title("AUT NORTH SHORE CAMPUS"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(SouthAUT, 10.2f));
     }
 }
