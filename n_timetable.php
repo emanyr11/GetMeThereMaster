d0f846255eb3da583ebab9a32ab2cfec7b6555db
llNitishAnandll <nz.nitish@hotmail.co.nz>
2017-06-09 00:09:55 +1200

    Add files via upload
    Database Files

---
+++ n_timetable.php
@@ -0,0 +1,18 @@
+<?php
+	$host= "localhost";
+	$user = "id1523917_getmethere";
+	$password = "getmethere";
+	$db= "id1523917_getmethere";
+	$sql = "SELECT * FROM n_timetable;";
+	$conn = mysqli_connect($host, $user, $password, $db);
+	
+        $result = mysqli_query($conn,$sql);
+		$response = array();
+        while($row=mysqli_fetch_array($result))
+	{
+	array_push($response,array("Class Start"=>$row[1],"Class Finish"=>$row[2], "Location"=>$row[3],
+        "Paper"=>$row[4]));
+	}
+	echo json_encode(array("server_response"=>$response));
+	mysqli_close($conn);
+?>
